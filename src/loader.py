5001a3b9368f6855dae61529bb457d515e6809d8
Mkarti <kartikmishra1225@gmail.com>
2026-06-30 22:24:30 +0530

    Remove debug print from loader

--- src/loader.py
+++ src/loader.py
@@ -4,11 +4,6 @@ import streamlit as st
 
 @st.cache_data
 def load_data():
-
     df = pd.read_csv("data/Atlantic_United_States.csv")
-
-    print(df.columns.tolist())
-
     df["date"] = pd.to_datetime(df["date"])
-
     return df
